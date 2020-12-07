5426088f85ac98ab3fe41990a30ee04510e4b4d3
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-07 17:57:14 +0800

    editing code

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+
+
+
+    return (
+        <div>
+            <h1 className="mt-5">React Testing Application Website</h1>
+        </div>
+
+    );
+
+}
+
+export default Home;
