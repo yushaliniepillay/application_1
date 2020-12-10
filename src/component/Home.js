9115785be4557f5786c2acdd488c637a565871f2
yushaliniepillay <yushalinie97@hotmail.com>
2020-12-10 14:54:52 +0800

    update code

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomePageImage from '../component/HomePageImage';
+import NavBar from'../component/NavBar';
+
 
 const Home = () => {
 
@@ -7,6 +9,8 @@ const Home = () => {
 
     return (
         <div>
+            <NavBar />
+            <br></br>
             <HomePageImage />
             <h1 className="mt-5">React Testing Application Website</h1>
         </div>
---
+++ src/component/NavBar.js
@@ -0,0 +1,22 @@
+import {Nav, Navbar, Button, Form, FormControl} from 'react-bootstrap';
+
+const NavBar = () => {
+
+
+    return (
+        <Navbar bg="primary" variant="dark" fixed="top">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-light">Search</Button>
+            </Form>
+        </Navbar>
+    );
+}
+
+export default NavBar;
